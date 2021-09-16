2f18e056c93662214a548d63e5faeb876712a3f1
hashi654 <clsy.engineering@gmail.com>
2021-09-16 22:23:13 +0900

    itemモデルにバリデーションを記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,13 @@
 class Item < ApplicationRecord
+  validates :item_name, presence: true
+  validates :explanation, presence: true
+  validates :category_id, presence: true
+  validates :status_id, presence: true
+  validates :shipping_charge_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :days_to_ship_id, presence: true
+  validates :image, presence: true
+
   belongs_to :user
   has_one_attached :image
 end
