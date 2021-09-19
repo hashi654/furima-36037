0344f12c741677739b9f1f92680fbc0ba6329a06
hashi654 <clsy.engineering@gmail.com>
2021-09-19 22:40:43 +0900

    category.ebの修正、newビューファイルの修正、itemsコントローラーにnew、createアクションを記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,14 +3,21 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private 
   
   def item_params
-    params.require(:message).permit(:image).merge(user_id: current_user.id)
+    params.require(:item).permit(:item_name, :explanation, :category_id, :status_id, :shipping_charge_id, :prefecture_id, :days_to_ship_id, :price, :image).merge(user_id: current_user.id)
   end
 end
