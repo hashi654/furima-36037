4b9bec6f7a3d5074a160511830eb92d19e9dc10e
hashi654 <clsy.engineering@gmail.com>
2021-09-19 22:24:08 +0900

    genre.rbからcategory.rbに名前変更しカテゴリー入力

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   validates :item_name, presence: true
   validates :explanation, presence: true
-  validates :category_id, presence: true
+  validates :category_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :status_id, presence: true
   validates :shipping_charge_id, presence: true
   validates :prefecture_id, presence: true
@@ -10,4 +10,8 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+
 end
