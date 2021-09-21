6f78bb26c384eb441287f66d38243aebedbfb0af
hashi654 <clsy.engineering@gmail.com>
2021-09-21 22:58:41 +0900

    factorybotの作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    image           {'app/assets/images/flag.png'}
+    item_name       { 'MacBookAir'}
+    explanation     { 'MacBookAirの2020年モデルです。新モデルを購入したので不要となりました' }
+    category_id     { '2' }
+    status_id       { '2' }
+    cost_id         { '2' }
+    prefecture_id   { '2'}
+    days_id         { '2' }
+    price           { '100000' }
   end
 end
