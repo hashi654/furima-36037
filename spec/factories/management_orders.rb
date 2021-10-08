fb58d77ceb67adb5a8bbcb49a13f96355576a468
hashi654 <clsy.engineering@gmail.com>
2021-10-08 23:19:44 +0900

    FactoryBotの作成

--- spec/factories/management_orders.rb
+++ spec/factories/management_orders.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
   factory :management_order do
-    
+    postal_code { '123-4567'}
+    prefecture_id { 2 }
+    municipality { '岡山市' }
+    address { '岡山駅前' }
+    building { 'シャインブライト200'}
+    telephone_number { 09020002000 }
   end
 end
